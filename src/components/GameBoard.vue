57cc78fd86f5d766d6f263a2b641f270f0e72b23
Sergey Nagornykh <serega1905@ukr.net>
2024-03-19 15:12:52 +0200

    clean

---
+++ src/components/GameBoard.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template><div>Game Board</div></template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Hello world</h1>
+  </div>
 </template>
+
+<style lang="scss" scoped></style>
