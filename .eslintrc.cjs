dbb587c7dd749d65c777c90f404850948ac6e4d7
Sergey Nagornykh <serega1905@ukr.net>
2024-03-20 10:40:47 +0200

    eslint no relative imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,26 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  plugins: ['no-relative-import-paths'],
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  rules: {
+    'no-relative-import-paths/no-relative-import-paths': [
+      'error',
+      { rootDir: 'src', prefix: '@' }
+    ],
+  },
+  overrides: [
+    {
+      files: ['vitest.config.ts'],
+      rules: {
+        'no-relative-import-paths/no-relative-import-paths': 'off'
+      }
+    }
   ],
   parserOptions: {
     ecmaVersion: 'latest'
