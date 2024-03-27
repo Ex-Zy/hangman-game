a25e08c5fdc5476cfe9de10a0097d7f49a0f3bde
Sergey Nagornykh <serega1905@ukr.net>
2024-03-27 14:19:51 +0200

    feat: KeyboardLetter

--- src/components/InGameBoard/GameKeyboard.vue
+++ src/components/InGameBoard/GameKeyboard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
 
+import KeyboardLetter from '@/components/InGameBoard/KeyboardLetter.vue'
 import { useGame } from '@/stores/useGame'
 
 const { keyboardAlphabet } = storeToRefs(useGame())
@@ -9,14 +10,16 @@ const { pickLetter } = useGame()
 
 <template>
   <article class="keyboard">
-    <span
-      v-for="(letter, key) in keyboardAlphabet"
-      :key="key"
-      style="padding: 10px; border: 1px solid red; display: inline-flex"
-      @click="pickLetter(letter)"
-      >{{ key }}</span
-    >
+    <template v-for="letter in keyboardAlphabet" :key="letter.name">
+      <KeyboardLetter :letter="letter" @click="pickLetter(letter)" />
+    </template>
   </article>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.keyboard {
+  --gap: 8px;
+
+  @include flex-row($gap: var(--gap));
+}
+</style>
---
+++ src/components/InGameBoard/KeyboardLetter.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+import type { Letter } from '@/types'
+
+interface Props {
+  letter: Letter
+}
+
+const props = defineProps<Props>()
+const classes = computed(() => {
+  const root = 'letter'
+  const disable = 'disable'
+
+  return {
+    [root]: true,
+    [`${root}--${disable}`]: !props.letter.enable
+  }
+})
+</script>
+
+<template>
+  <div :class="classes">
+    {{ props.letter.name }}
+  </div>
+</template>
+
+<style scoped lang="scss">
+.letter {
+  --width: 28px;
+  --height: 56px;
+  --radius: 8px;
+  --font-size: 24px;
+
+  @include sizer(var(--width), var(--height));
+  @include flex-center;
+  @include font($fs: var(--font-size), $color: var(--color-dark-navy));
+
+  cursor: pointer;
+  background: var(--color-white);
+  border-radius: var(--radius);
+
+  &--disable {
+    background: rgba(255, 255, 255, 0.25);
+  }
+}
+</style>
