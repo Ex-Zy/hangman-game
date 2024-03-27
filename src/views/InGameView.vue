985da4ea893a1e19f20a91860a7c2ae948f5355a
Sergey Nagornykh <serega1905@ukr.net>
2024-03-27 14:28:26 +0200

    update: correct title for InGameView

--- src/views/InGameView.vue
+++ src/views/InGameView.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import InGameBoard from '@/components/InGameBoard/InGameBoard.vue'
 import InGameLayout from '@/layouts/InGameLayout.vue'
+import { useCategories } from '@/stores/useCategories'
+
+const categoriesStore = useCategories()
 </script>
 
 <template>
-  <InGameLayout title="Countries">
+  <InGameLayout :title="categoriesStore.category">
     <InGameBoard />
   </InGameLayout>
 </template>
