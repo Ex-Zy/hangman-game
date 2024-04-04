e069bc7eebf0f2bade846012505b6374062aa80a
Sergey Nagornykh <serega1905@ukr.net>
2024-04-04 15:06:34 +0300

    :heavy_plus_sign: update: eslint-plugin-vuejs-accessibility

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  plugins: ['no-relative-import-paths', 'simple-import-sort'],
+  plugins: ['no-relative-import-paths', 'simple-import-sort', 'vuejs-accessibility'],
   extends: [
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
@@ -11,7 +11,8 @@ module.exports = {
     'plugin:@typescript-eslint/stylistic-type-checked',
     '@vue/eslint-config-typescript/recommended',
     '@vue/eslint-config-prettier/skip-formatting',
-    'plugin:vue-scoped-css/vue3-recommended'
+    'plugin:vue-scoped-css/vue3-recommended',
+    "plugin:vuejs-accessibility/recommended",
   ],
   rules: {
     'no-relative-import-paths/no-relative-import-paths': ['error', { rootDir: 'src', prefix: '@' }],
