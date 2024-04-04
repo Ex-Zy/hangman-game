2737d8fa7adc6eea62e111f0af4618b5a944ab3f
Sergey Nagornykh <serega1905@ukr.net>
2024-04-05 01:07:52 +0300

    :lipstick: update: HealthBar animation

--- src/components/InGameBoard/HealthBar.vue
+++ src/components/InGameBoard/HealthBar.vue
@@ -1,14 +1,41 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue'
+
 interface Props {
   value?: number
 }
 const props = withDefaults(defineProps<Props>(), {
   value: 0
 })
+
+const indicatorRef = ref<HTMLDivElement>()
+const actions = ref<(() => void)[]>([])
+const DURATION = 1000
+
+function animate() {
+  indicatorRef.value?.classList.add('shake-horizontal')
+  setTimeout(() => {
+    indicatorRef.value?.classList.remove('shake-horizontal')
+  }, DURATION)
+}
+
+function pushActions(health: number, oldHealth: number) {
+  if (health < oldHealth) {
+    actions.value.push(animate)
+  }
+}
+
+function popActions() {
+  const firstAction = actions.value.shift()
+  firstAction?.()
+}
+
+watch(() => props.value, pushActions)
+watch(() => actions.value.length, popActions)
 </script>
 
 <template>
-  <div class="progress">
+  <div ref="indicatorRef" class="progress">
     <div class="progress__indicator" :style="{ width: `${props.value}%` }"></div>
   </div>
 </template>
@@ -43,6 +70,44 @@ const props = withDefaults(defineProps<Props>(), {
     height: var(--indicator-height);
     background: var(--color-dark-navy);
     border-radius: var(--radius);
+    transition: width 1s;
+  }
+}
+
+/**
+ * ----------------------------------------
+ * animation shake-horizontal
+ * ----------------------------------------
+ */
+.shake-horizontal {
+  animation: shake-horizontal 0.8s cubic-bezier(0.455, 0.03, 0.515, 0.955) both;
+}
+
+@keyframes shake-horizontal {
+  0%,
+  100% {
+    transform: translateX(0);
+  }
+
+  10%,
+  30%,
+  50%,
+  70% {
+    transform: translateX(-10px);
+  }
+
+  20%,
+  40%,
+  60% {
+    transform: translateX(10px);
+  }
+
+  80% {
+    transform: translateX(8px);
+  }
+
+  90% {
+    transform: translateX(-8px);
   }
 }
 </style>
